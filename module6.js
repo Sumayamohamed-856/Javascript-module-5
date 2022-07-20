75326543ef4eb54e37150ec80d8576439eae061d
Sumayamohamed <Fahimaqalanjo818@gmail.com>
2022-07-19 19:35:48 -0700

    updated

---
+++ module6.js
@@ -0,0 +1,25 @@
+function fullName(first, last) {
+    return `${first} ${last}`;
+  }
+  
+  fullName('Kristine', 'Hudgens');
+  
+  
+  newFullName = (first, last) => {
+    return `${first} ${last}`;
+  }
+  
+  newFullName('Kristine', 'Hudgens');
+
+
+
+  // Source page: http://www.dailysmarty.com/
+
+// Console with Command Line API / Firebug
+$('.image-item')
+$('.image-item').first // returns function
+$('.image-item').first()
+$('.image-item').first().hide()
+
+// Compared with
+document.getElementsByClassName('post-item-wrapper')[0].style.display = 'none';
